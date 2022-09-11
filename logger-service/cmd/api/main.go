f879aab49cb1ce98e76e5019d243c49b02f621d7
Rahul Dogra <rahuldogra1998@gmail.com>
2022-09-11 05:45:23 +0530

    mongo connection fixed

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -15,7 +15,7 @@ import (
 const (
 	webPort  = "80"
 	rpcPort  = "5001"
-	mongoURI = "monogodb://localhost:27017"
+	mongoURL = "mongodb://mongo:27017"
 	gRpcPort = "50001"
 )
 
@@ -27,20 +27,17 @@ type Config struct {
 
 func main() {
 	// * connect to mongo
-
 	mongoClient, err := connectToMongo()
 	if err != nil {
 		log.Panic(err)
 	}
-
 	client = mongoClient
 
-	// * create a context in order to disconnct
-
+	// * create a context in order to disconnect
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	// * close conenction
+	// * close connection
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
@@ -52,36 +49,35 @@ func main() {
 	}
 
 	// * start web server
-	go app.serve()
-}
-
-func (app *Config) serve() {
+	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+
 }
 
 func connectToMongo() (*mongo.Client, error) {
 	// * create connection options
-
-	clientOptions := options.Client().ApplyURI(mongoURI)
-
+	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
 		Username: "admin",
 		Password: "password",
 	})
 
+	// * connect
 	c, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Panicln("Error connecting", err)
+		log.Println("Error connecting:", err)
 		return nil, err
 	}
+
+	log.Println("Connected to mongo!")
 
 	return c, nil
 }
