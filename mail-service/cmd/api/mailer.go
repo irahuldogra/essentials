742aab411b0906ee193045938f9fadcb7adb6281
Rahul Dogra <rahuldogra1998@gmail.com>
2022-09-12 23:41:46 +0530

    :adhesive_bandage: Go template style changed to dark

---
+++ mail-service/cmd/api/mailer.go
@@ -0,0 +1,22 @@
+package main
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FormAddress string
+	FromName    string
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        any
+	DataMap     map[string]any
+}
